bf0466a08a9b22017cba29c1d2ee6186707d699c
Andreas Rasmussen <andreasrefras@gmail.com>
2018-01-30 18:08:09 +0100

    Major restructure of project files to get automatic cache busting (via a grunt package) when building -> grunt-contrib-copy copies all non .css or .js files from public to dist folder and then grunt-cache-bust puts random hash in the build .css and .js filenames and updates references to these files in all html-files in dist. From now on html changes shall be done in the public folder and css and js changes in the src folder. When ready to deploy run build and take files from dist folder to the server

---
+++ dist/assets/bundle.53e0400ce32c34c2.js
@@ -0,0 +1,27 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var projectIntro = document.getElementById('projectIntro');
+
+if (projectIntro) {
+  window.addEventListener('scroll', function () {
+    var scrollTop = window.pageYOffset !== undefined ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    window.setTimeout(function () {
+      if (scrollTop > 200) {
+        projectIntro.className = "fadeOut";
+      } else {
+        projectIntro.className = "visible";
+      }
+    }, 100);
+  });
+}
+
+// Wating a few seconds before replaying kommunernes-teknologispring-matrix-click video
+document.getElementById('kommunernesTeknologispringMatrixClickVideo').addEventListener('ended', replayAfterXSeconds, false);
+function replayAfterXSeconds(e) {
+  setTimeout(function () {
+    document.getElementById('kommunernesTeknologispringMatrixClickVideo').play();
+  }, 1500);
+}
+
+},{}]},{},[1]);
--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 module.exports = function(grunt) {
 
 	// 1. Configure tasks
@@ -6,12 +8,20 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON( 'package.json' ),
 
 		browserify: {
-      dist: {
+      dev: {
         options: {
           transform: [['babelify', { 'sourceType': 'module' }]]
         },
         files: {
-          'dist/bundle.js': 'src/scripts/**/*.js'
+          'public/assets/bundle.js': 'src/scripts/**/*.js'
+        }
+      },
+      build: {
+        options: {
+          transform: [['babelify', { 'sourceType': 'module' }]]
+        },
+        files: {
+          'dist/assets/bundle.js': 'src/scripts/**/*.js'
         }
       }
     },
@@ -23,7 +33,7 @@ module.exports = function(grunt) {
 					outputStyle: 'expanded' // 'expanded' = makes the output .css-file easy to read
 				},
 				files: {
-					'dist/style.css': 'src/styles/style.scss'
+					'public/assets/style.css': 'src/styles/style.scss'
 				}
 			},
 			build: {
@@ -31,51 +41,40 @@ module.exports = function(grunt) {
 					outputStyle: 'compressed' // 'compressed' = minifies the output for live server
 				},
 				files: {
-					'dist/style.css': 'src/styles/style.scss'
+					'dist/assets/style.css': 'src/styles/style.scss'
 				}
 			}
 		},
 
-    autoprefixer:{
-      dist:{
-        files:{
-          'dist/style.css':'dist/style.css'
+    autoprefixer: {
+      dev: {
+        files: {
+          'public/assets/style.css': 'public/assets/style.css'
+        }
+      },
+      build: {
+        files: {
+          'dist/assets/style.css':'dist/assets/style.css'
         }
       }
     },
 
-    // Initialize 'grunt watch'-task with livereload (also install livereload plugin in browser to make it work)
-		watch: {
-      options: { livereload: true },
-			js: {
-				files: [ 'src/scripts/**/*.js' ],
-				tasks: [ 'browserify' ] // Concatenate scripts on change, but don't minify while developing (see options under 'uglify' ^)
-			},
-			css: {
-        files: [ 'src/styles/**/*.scss' ],
-				tasks: [ 'sass:dev' ] // Concatenate styles on change, but don't minify while developing (see options under 'sass' ^)
-			},
-      livereload: {
-        files: [ '*.html', 'dist/assets/images/**/*.{png,jpg,jpeg,gif,webp,svg}' ]
-      }
-		},
-
-    express:{
-      all:{
-        options:{
+    express: {
+      server: {
+        options: {
           port: 3000,
           hostname: 'localhost',
-          bases: ['./'],
+          bases: [ 'public' ],
           livereload: true
         }
       }
     },
 
-    chokidar: {
+    chokidar: { // Like grunt-contrib-watch
       options: { livereload: true },
       scripts: {
         files: [ 'src/scripts/**/*.js' ],
-        tasks: [ 'browserify' ],
+        tasks: [ 'browserify:dev' ],
         options: {
           spawn: false,
           interrupt: true
@@ -83,10 +82,32 @@ module.exports = function(grunt) {
       },
       css: {
         files: [ 'src/styles/**/*.scss' ],
-        tasks: [ 'sass:dev', 'autoprefixer' ],
+        tasks: [ 'sass:dev', 'autoprefixer:dev' ],
       },
       livereload: {
-        files: [ '*.html', 'images/**/*.{png,jpg,jpeg,gif,webp,svg}' ]
+        files: [ 'public/*.html', 'public/assets/**/*.{png,jpg,jpeg,gif,webp,svg}' ]
+      }
+    },
+
+    copy: {
+      main: {
+        files: [
+          { expand: true, cwd: 'public/assets/icons', src: [ '**' ], dest: 'dist/assets/icons' },
+          { expand: true, cwd: 'public/assets/illustrations', src: [ '**' ], dest: 'dist/assets/illustrations' },
+          { expand: true, cwd: 'public/assets/images', src: [ '**' ], dest: 'dist/assets/images' },
+          { expand: true, cwd: 'public', src: [ '*.html', '*.txt' ], dest: 'dist' }
+        ]
+      }
+    },
+
+    cacheBust: {
+      build: {
+        options: {
+          baseDir: './dist/assets/',
+          assets: [ 'bundle.js', 'style.css' ],
+          deleteOriginals: true
+        },
+        src: [ './dist/*.html' ]
       }
     }
 
@@ -96,12 +117,13 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks( 'grunt-browserify' );
   grunt.loadNpmTasks( 'grunt-express' );
   grunt.loadNpmTasks( 'grunt-sass' );
-  grunt.loadNpmTasks( 'grunt-contrib-watch' ); // Runs on command 'grunt watch'
   grunt.loadNpmTasks( 'grunt-chokidar' ); // Faster alternative to watch task
   grunt.loadNpmTasks( 'grunt-autoprefixer' );
+  grunt.loadNpmTasks( 'grunt-contrib-copy' );
+  grunt.loadNpmTasks( 'grunt-cache-bust' );
 
   // 3. Register task(s)
   grunt.registerTask( 'serve', [ 'express', 'chokidar' ]); // Runs also on command 'grunt' as it is set to default
-  grunt.registerTask( 'uglify', [ 'sass:dev', 'autoprefixer' ]); // Runs on command 'grunt uglify'
-  grunt.registerTask( 'build', [ 'sass:build', 'autoprefixer', 'browserify' ]); // Runs on command 'grunt build'
+  grunt.registerTask( 'uglify', [ 'sass:dev', 'autoprefixer:dev' ]); // Runs on command 'grunt uglify'
+  grunt.registerTask( 'build', [ 'sass:build', 'autoprefixer:build', 'browserify:build', 'copy', 'cacheBust:build' ]); // Runs on command 'grunt build'
 };
